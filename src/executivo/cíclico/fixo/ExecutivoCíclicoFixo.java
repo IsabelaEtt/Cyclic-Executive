7db0d3cfffdfd0936f94bce541a84acf8d0e05ce
Isabela <49378594+IsabelaEtt@users.noreply.github.com>
2022-05-13 08:08:43 -0300

    First commit

---
+++ src/executivo/cíclico/fixo/ExecutivoCíclicoFixo.java
@@ -0,0 +1,9 @@
+package executivo.cíclico.fixo;
+
+// @author Isabela Canelas Ett - RA00303107
+
+public class ExecutivoCíclicoFixo {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
